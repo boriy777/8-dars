17252a5ef4e0f510c7be46f8fc00e556858cc26c
boriy777 <abdulboriy822@gmail.com>
2024-11-08 17:18:45 +0500

    Add files via upload

---
+++ scripe.js
@@ -0,0 +1,44 @@
+// let a = prompt('ism kiriting')
+
+// console.log(`sizning ismingizda ${a.length} ta harif bor`)
+
+
+// let a = +prompt('son kiriting')
+
+
+// if(a%2== 0) {
+
+//     console.log('juft son')
+
+
+// }else{
+//     console.log('toq son')
+// }
+
+
+
+let baho = prompt('xarf tanlang\n A \n B\n C')
+
+if( baho = "A") {
+    console.log('siz alochisiz')
+
+}else if (baho == "B") {
+    console.log("siz yaxshi bahoga oqiysiz")
+
+}else if (baho == "C"){
+    console.log("siz yomon bahoga oqiysiz")
+
+}else {
+    console.log("siz kalidorda oqisiz")
+}
+
+
+
+
+
+
+
+
+
+
+
